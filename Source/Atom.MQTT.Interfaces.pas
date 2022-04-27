4456117ef47a85730cd1f77f943393b215a5c886
SamJokO <samjoko@gmail.com>
2022-04-27 16:52:40 +0900

    소켓 인터페이스 정의

--- Source/Atom.MQTT.Interfaces.pas
+++ Source/Atom.MQTT.Interfaces.pas
@@ -2,7 +2,15 @@
 
 interface
 
+uses
+  System.SysUtils;
+
 type
+  IMQTTSocket = interface ['{2FD72D7F-E950-4592-B9E0-C9176B6B483B}']
+    procedure                   SendBuffer(ABuffer: TBytes);
+//    function                    Recv
+  end;
+
   IMQTTClient = interface ['{9DB085CC-BA0F-44D6-B06D-25DDEC7DC255}']
   end;
 
