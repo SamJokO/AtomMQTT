8a4ba00490f676e96bff132da42d890198bb44ae
SamJokO <samjoko@gmail.com>
2022-04-27 16:43:12 +0900

    Qos Type 정의, Variable Header 정의

--- Source/Atom.MQTT.Headers.pas
+++ Source/Atom.MQTT.Headers.pas
@@ -2,6 +2,9 @@
 
 interface
 
+uses
+  System.SysUtils;
+
 type
   TMQTTPacketType             = (
                                   Reserved,           // 0 : Reserved
@@ -22,8 +25,18 @@ type
                                   AUTH                // 15 : Client to Server or Server to Client, Authentication exchange
                                 );
 
-{
+  TMQTTQoSType                = (
+                                  AT_MOST_ONCE,       // 0
+                                  AT_LEAST_ONCE,      // 1
+                                  EXACTLY_ONCE        // 2
+                                );
 
+{
+                       |   7  |  6  |  5  |  4  |  3  |  2  |  1  |  0  |
+  FixedHeader Byte(1)  |   Control Packet Type  |        Flags          |
+             Byte(2~n) |   Remaining Length (1 ~ 4byte, Total Length    | (without FixhedHeader Length)
+  VariableHeader (3~n) |   Optional: Variable Length Header             |
+  Payload      (n+1~m) |   Optional: Variable Length Message Payload    |
 }
 
   TMQTTFlags = packed record
@@ -56,6 +69,77 @@ type
     property                    Reserved: Integer index $0001 read GetBits write SetBits;
 
   end;
+
+  TMQTTVariableHeader = class
+  private
+    FBytes                      : TBytes;
+  protected
+    procedure                   AddField(AValue: Byte); overload;
+    procedure                   AddField(AValues: TBytes); overload;
+    procedure                   ClearField;
+  public
+    constructor                 Create;
+    function                    ToBytes: TBytes; virtual;
+  end;
+
+const
+  RC_SUCCESS                  = $00;
+  RC_NORMAL_DISCONNECTION     = $00;
+  RC_GRANTED_QOS_0            = $00;
+  RC_GRANTED_QOS_1            = $01;
+  RC_GRANTED_QOS_2            = $02;
+  RC_DISCONNECT_WITH_WILL_MESSAGE
+                              = $04;
+
+  RC_NO_MATCHING_SUBSCRIBER   = $10;
+  RC_NO_SUBSCRIPTION_EXISTED  = $11;
+
+  RC_CONTINUE_AUTHENTICATION  = $18;
+  RC_RE_AUTHENTICATE          = $19;
+
+  RC_UNSPECIFIED_ERROR        = $80;
+  RC_MALFORMED_PACKET         = $81;
+  RC_PROTOCOL_ERROR           = $82;
+  RC_IMPLEMENTATION_SPECIFIC_ERROR
+                              = $83;
+  RC_UNSUPPORTED_PROTOCOL_VERSION
+                              = $84;
+  RC_CLIENT_IDENTIFIER_NOT_VALID
+                              = $85;
+  RC_BAD_USER_NAME_OR_PASSWORD= $86;
+  RC_NOT_AUTHORIZED           = $87;
+  RC_SERVER_UNAVAILABLE       = $88;
+  RC_SERVER_BUSY              = $89;
+  RC_BANNED                   = $8A;
+  RC_SERVER_SHUTTING_DOWN     = $8B;
+  RC_BAD_AUTHENTICATION_METHOD= $8C;
+  RC_KEEP_ALIVE_TIMEOUT       = $8D;
+  RC_SESSION_TAKEN_OVER       = $8E;
+  RC_TOPIC_FILTER_INVALID     = $8F;
+  RC_TOPIC_NAME_INVALID       = $90;
+  RC_PACKET_IDENTIFIER_IN_USE = $91;
+  RC_PACKET_IDENTIFIER_NOT_FOUND
+                              = $92;
+  RC_RECEIVE_MAXIMUM_EXCEEDED = $93;
+  RC_TOPIC_ALIAS_INVALID      = $94;
+  RC_PACKET_TOO_LARGE         = $95;
+  RC_MESSAGE_RATE_TOO_HIGH    = $96;
+  RC_QUOTA_EXCEEDED           = $97;
+  RC_ADMINISTRATIVE_ACTION    = $98;
+  RC_PAYLOAD_FORMAT_INVALID   = $99;
+  RC_RETAIN_NOT_SUPPORTED     = $9A;
+  RC_QOS_NOT_SUPPORTED        = $9B;
+  RC_USE_ANOTHER_SERVER       = $9C;
+  RC_SERVER_MOVED             = $9D;
+  RC_SHARED_SUBSCRIPTIONS_NOT_SUPPORTED
+                              = $9E;
+  RC_CONNECTION_RATE_EXCEEDED = $9F;
+  RC_MAXIMUM_CONNECT_TIME     = $A0;
+  RC_SUBSCRIPTION_IDENTIFIERS_NOT_SUPPORTED
+                              = $A1;
+  RC_WILDCARD_SUBSCRIPTIONS_NOT_SUPPORTED
+                              = $A2;
+
 
 implementation
 
@@ -98,6 +182,41 @@ end;
 procedure TMQTTConnectFlags.SetBits(const AIndex, AValue: Integer);
 begin
   SetDWordBits(Flags, AIndex, AValue);
+end;
+
+{ TMQTTVariableHeader }
+
+procedure TMQTTVariableHeader.AddField(AValue: Byte);
+var
+  vLength                     : Integer;
+begin
+  vLength                     := Length(FBytes);
+  SetLength(FBytes, vLength + SizeOf(AValue));
+  Move(AValue, FBytes[vLength], SizeOf(AValue));
+end;
+
+procedure TMQTTVariableHeader.AddField(AValues: TBytes);
+var
+  vLength                     : Integer;
+begin
+  vLength                     := Length(FBytes);
+  SetLength(FBytes, vLength + Length(AValues));
+  Move(AValues[0], FBytes[vLength], Length(AValues));
+end;
+
+procedure TMQTTVariableHeader.ClearField;
+begin
+  SetLength(FBytes, 0);
+end;
+
+constructor TMQTTVariableHeader.Create;
+begin
+  //
+end;
+
+function TMQTTVariableHeader.ToBytes: TBytes;
+begin
+  Result                      := FBytes;
 end;
 
 end.
--- Source/Atom.MQTT.Interfaces.pas
+++ Source/Atom.MQTT.Interfaces.pas
@@ -6,6 +6,9 @@ type
   IMQTTClient = interface ['{9DB085CC-BA0F-44D6-B06D-25DDEC7DC255}']
   end;
 
+  IMQTTBroker = interface ['{2E9B6239-22AA-461F-BD56-6723E4255F60}']
+  end;
+
 implementation
 
 end.
